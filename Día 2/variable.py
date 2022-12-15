7bdfaf93babc1d00eeb7dac7777f4eb7f8cc62f1
Luis <pepevilluela06@gmail.com>
2022-12-15 09:14:12 +0100

    Día 2

---
+++ Día 2/variable.py
@@ -0,0 +1,28 @@
+#Day 2: 30 Days of python programming
+
+first_name ='Luis'
+last_name ='Juarez'
+full_name ='Luis Juarez'
+country ='Spain'
+city ='Jerez'
+age ='16'
+year ='2022'
+is_married ='True'
+is_true ='False'
+is_light_on ='Yes'
+first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on =
+ 'Luis', 'Juarez', 'Luis Juarez', 'Spain', 'Jerez', '16', '2022', 'True', 'False', 'Yes'
+
+ print(type(first_name))
+ print(type(last_name))
+ print(type(full_name))
+ print(type(country))
+ print(type(city))
+ print(type(age))
+ print(type(year))
+ print(type(is_married))
+ print(type(is_true))
+ print(type(is_light_on))
+
+print(len(first_name))
+print(len(first_name==last_name))
