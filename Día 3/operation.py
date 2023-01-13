ac6a5c0877454f0190279f84f09e84b75440c75d
Luis <pepevilluela06@gmail.com>
2023-01-13 11:12:42 +0100

    Día 3

--- Día 3/operation.py
+++ Día 3/operation.py
@@ -14,3 +14,17 @@ print(area)
 side_a = 5
 side_b = 4
 side_c = 3
+perimeter = side_a + side_b + side_c
+
+width = 3
+lenght = 4
+area_of_rectangule = width * lenght
+perimeter_of_rectangule = (width + lenght) * 2
+print (perimeter_of_rectangule , area_of_rectangule)
+
+radius = 6
+pi = 3.14 
+area_of_circle = pi * radius * radius
+circumference = 2 * pi * radius
+print (area_of_circle , circumference)
+
