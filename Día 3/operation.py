8efedb00f2221d340164378b86fd6275f7304e30
Luis <pepevilluela06@gmail.com>
2023-01-13 11:11:26 +0100

    Día 1

---
+++ Día 3/operation.py
@@ -0,0 +1,16 @@
+age=16
+print(age)
+
+height= 1,80
+print ('Floating point variable')
+complex = 1 + 1j
+print ('complex number:', 1 + 1j)
+
+base = 20
+height = 10
+area = 0.5 * base * height
+print(area)
+
+side_a = 5
+side_b = 4
+side_c = 3
