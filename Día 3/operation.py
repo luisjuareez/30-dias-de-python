037c0ad1514f026b4953502ca2c8d309c93a0382
Luis <pepevilluela06@gmail.com>
2023-02-23 09:14:40 +0100

    Día 4

--- Día 3/operation.py
+++ Día 3/operation.py
@@ -2,7 +2,7 @@ age=16
 print(age)
 
 height= 1,80
-print ('Floating point variable')
+print ('Floating point variable', height, 1,80)
 complex = 1 + 1j
 print ('complex number:', 1 + 1j)
 
@@ -15,6 +15,7 @@ side_a = 5
 side_b = 4
 side_c = 3
 perimeter = side_a + side_b + side_c
+print(perimeter)
 
 width = 3
 lenght = 4
@@ -28,3 +29,23 @@ area_of_circle = pi * radius * radius
 circumference = 2 * pi * radius
 print (area_of_circle , circumference)
 
+x=-2
+y=2
+y = (2*(x-2))
+slope8= y
+print(slope8)
+
+x1= 2
+y1= 2
+x2= 6
+y2= 10
+m= y2-y1/x2-x1
+slope9= m 
+print (slope9)
+comparation= (slope9==slope8)
+print(comparation)
+euclidian_distance= (y2-y1 + x2-x1)**0.5 * 2
+print(euclidian_distance)
+
+x= input ('cuanto es el valor de x')
+y= (x ^ 2)+(6 * x)+ 9
