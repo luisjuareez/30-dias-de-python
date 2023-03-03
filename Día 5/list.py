5b3dceb5c991aa871d17d9ca88d203159905c27e
Luis <pepevilluela06@gmail.com>
2023-03-03 11:13:56 +0100

    Día 5 y 6 acabados

--- Día 5/list.py
+++ Día 5/list.py
@@ -24,6 +24,25 @@ it_companies[1]=it_companies[1].upper()
 print('joined list: t/', '#;'.join(it_companies)) #13
 it_companies= ['Facebook', 'Google', 'Microsoft', 'Apple', 'IDM', 'Oracle', 'Amazon']
 does_exist= 'Amazon' in it_companies
-print(does_exist) #15
-it_companies.sort()
-it_companies.reverse()
+print(does_exist) 
+it_companies.sort() #16
+it_companies.reverse() #17
+
+it_companies= ['Facebook', 'Google', 'Microsoft', 'Apple', 'IDM', 'Oracle', 'Amazon'] #18
+all_companies= it_companies[0:6]
+facebook_google_microsoft= it_companies[0:2]
+print(facebook_google_microsoft)
+idm_oracle_amazon= it_companies[4:6] #19
+print(idm_oracle_amazon)
+it_companies[len(it_companies)//2] #20
+it_companies.pop(0) #21
+it_companies.pop(3) #22
+it_companies.pop(6) #23
+it_companies.clear() #24
+del it_companies #25
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux'] #26
+back_end = ['Node','Express', 'MongoDB']
+full_stack= front_end.copy() #27
+full_stack.insert(4, 'Phyton', 'SQL')
+print(full_stack)
---
+++ Día 6/tuples.py
@@ -0,0 +1,11 @@
+empty_tuple= ()
+print(empty_tuple)
+sister_brother= ('Julia', 'Victor')
+print(sister_brother)
+siblings= sister_brother
+print(siblings)
+say= 'How many siblings do you have'
+len(siblings)
+mum_dad= ('Maria Gabriela','Jose Luis')
+family= siblings + mum_dad
+print(family)
