a20882b103e3a837fd7dba19c80487a2e42aa914
Luis <pepevilluela06@gmail.com>
2023-03-09 09:07:09 +0100

    Día 8

---
+++ Día 8/dictionary.py
@@ -0,0 +1,13 @@
+dog= {}
+dog['name']= 'Chop'
+dog['color']= 'Black'
+dog['breed']= 'Rottweiler'
+dog['legs']= '4'
+dog['age']= '6'
+print(dog)
+
+studen_dictionary= {'first_name': 'Luis', 'last_name' : 'Juarez', 'gender' : 'masculine', 'age':'16', 'material_status' : 'single', 'skills' : 'creative', 'country' : 'Spain', 'city' : 'Jerez', 'address' : 'Plaza del agua'}
+print(len(studen_dictionary))
+values_skill= studen_dictionary['skills'].values()
+print(values_skill)
+studen_dictionary.append('imaginative', 'agile')
