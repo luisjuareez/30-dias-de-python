d7b7a0610a675fcdd04326314b4b37dcb93765cd
Luis <pepevilluela06@gmail.com>
2023-03-10 11:08:48 +0100

    Día 8

--- Día 8/dictionary.py
+++ Día 8/dictionary.py
@@ -10,4 +10,19 @@ studen_dictionary= {'first_name': 'Luis', 'last_name' : 'Juarez', 'gender' : 'ma
 print(len(studen_dictionary))
 values_skill= studen_dictionary['skills'].values()
 print(values_skill)
-studen_dictionary.append('imaginative', 'agile')
+studen_dictionary.append('imaginative', 'agile') 
+ 
+keys_list= studen_dictionary.keys
+print(keys_list)
+values_list= studen_dictionary.values
+print(values_list)
+
+studen_dictionary= {'first_name': 'Luis', 'last_name' : 'Juarez', 'gender' : 'masculine', 'age':'16', 'material_status' : 'single', 'skills' : 'creative', 'country' : 'Spain', 'city' : 'Jerez', 'address' : 'Plaza del agua'}
+list= list(studen_dictionary.items())
+print(list) 
+print(studen_dictionary.items())
+
+studen_dictionary.pop('address')
+print(studen_dictionary)
+
+dict.clear(dog)
